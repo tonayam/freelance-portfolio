7d74eaad7194a0b62409aca60e7e5b48e00bb9d1
tonayam <tonayam99@gmail.com>
2023-06-08 14:07:12 +0100

    Footer Added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import { BsArrowUpRight } from 'react-icons/bs';
+import { FaInstagram, FaBehance } from 'react-icons/fa';
+import { FiTwitter } from 'react-icons/fi';
 
 const Footer = () => {
-  return <footer className='footer'></footer>;
+  return (
+    <footer className='footer'>
+      <div className='top'>
+        <h1>Let's Create</h1>
+        <h2>UNLEASHING THE POTENTIAL OF YOUR BRAND</h2>
+        <button className='white'>
+          GET IN TOUCH <BsArrowUpRight />
+        </button>
+      </div>
+
+      <div className='line'></div>
+
+      <div className='bottom'>
+        <div className='box'>
+          <div className='logo'></div>
+          <div className='socials'>
+            <div className='item'>
+              <div className='icon'>
+                <FaInstagram />
+              </div>
+              <span>Instagram</span>
+            </div>
+            <div className='item'>
+              <div className='icon'>
+                <FiTwitter />
+              </div>
+              <span>Twitter</span>
+            </div>
+            <div className='item'>
+              <div className='icon'>
+                <FaBehance />
+              </div>
+              <span>Behance</span>
+            </div>
+          </div>
+        </div>
+
+        <div className='box'></div>
+
+        <div className='box'></div>
+
+        <div className='box'></div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import Footer from '../../components/Footer/Footer';
 
 const Home = () => {
   return (
     <main className='home-page'>
       <Navbar />
+      <Footer />
     </main>
   );
 };
