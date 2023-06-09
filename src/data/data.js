106456ced9887c98b728deb169d9458ab2d3fb13
tonayam <tonayam99@gmail.com>
2023-06-09 10:37:56 +0100

    Updates

---
+++ src/data/data.js
@@ -0,0 +1,29 @@
+// OUR SERVICES
+const ourServices = [
+  {
+    title: `BRANDING`,
+    info: `Establish a strong brand identity that sets you apart.`,
+    btnLabel: `ABOUT BRANDING`,
+    link: ``,
+  },
+  {
+    title: `CAMPAIGNS`,
+    info: `Utilize data and analytics to optimize your marketing performance.`,
+    btnLabel: `ABOUT CAMPAIGNS`,
+    link: ``,
+  },
+  {
+    title: `E-COMMERCE`,
+    info: `Develop customized online stores that reflect your brand and vision.`,
+    btnLabel: `ABOUT E-COMMERCE`,
+    link: ``,
+  },
+  {
+    title: `SOCIAL MEDIA`,
+    info: `Create engaging and shareable content that drives brand awareness.`,
+    btnLabel: `ABOUT SOCIAL MEDIA`,
+    link: ``,
+  },
+];
+
+export { ourServices };
--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -5,6 +5,7 @@ import quantumOffice from '../../assets/quantum-office.png';
 import selectedWork from '../../assets/selected-work.png';
 import { BsArrowUpRight } from 'react-icons/bs';
 import electricBikes from '../../assets/electric-bikes.png';
+import { ourServices } from '../../data/data';
 
 const Home = () => {
   return (
@@ -22,7 +23,36 @@ const Home = () => {
 
       {/* FIRST SECTION */}
       {/* FIRST SECTION */}
-      {/* FIRST SECTION */}
+
+      {/* OUR SERVICES SECTION */}
+      <section className='our-services'>
+        <div className='info'>
+          <h2 className='title'>OUR SERVICES</h2>
+          <div className='services'>
+            {ourServices.map((service, serviceIndex) => {
+              const { btnLabel, info, link, title } = service;
+              return (
+                <div className='service' key={serviceIndex}>
+                  <h3>{title}</h3>
+                  <p>{info}</p>
+                  <div className='circular-btn-with-label'>
+                    <a
+                      href={link}
+                      target='_blank'
+                      rel='noreferrer'
+                      className='icon'
+                    >
+                      <BsArrowUpRight />
+                    </a>
+                    <span>{btnLabel}</span>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className='img'></div>
+      </section>
 
       {/* SELECTED WORKS */}
       <section className='selected-work'>
@@ -40,7 +70,7 @@ const Home = () => {
           <div className='info'>
             <h2>ELECTRIC BIKES LAGOS</h2>
             <p>A Nigerian based e-commerce store that sells electric bikes</p>
-            <div className='btn'>
+            <div className='circular-btn-with-label'>
               <a
                 href='https://www.electricbikeslagos.com/'
                 target='_blank'
@@ -67,7 +97,7 @@ const Home = () => {
               Website Redesign: Improving user experience and modernizing
               design.
             </p>
-            <div className='btn'>
+            <div className='circular-btn-with-label'>
               <div className='icon'>
                 <BsArrowUpRight />
               </div>
