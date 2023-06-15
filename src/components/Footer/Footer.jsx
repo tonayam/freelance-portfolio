a800ac07c84fcd6a537b98a0cdaa2d2c1de5d97a
deelaurence <laurie143pelumi@gmail.com>
2023-06-15 17:15:02 +0100

    gsap fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,73 @@
-import React from 'react';
+import React, {useEffect,useRef} from 'react';
 import { BsArrowUpRight } from 'react-icons/bs';
 import { FaInstagram, FaBehance } from 'react-icons/fa';
 import { FiTwitter } from 'react-icons/fi';
 import letsCreate from '../../assets/lets-create.png';
 
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger)
+
+
+
 const Footer = () => {
+  const bottomRef=useRef(null)
+  const bottom = bottomRef.current
+  useEffect(() => {
+  const twitter = document.querySelector('.twitter')
+  const insta = document.querySelector('.insta')
+  const behance = document.querySelector('.behance')
+  const bottomDom = document.querySelector('.bottom')
+        gsap.fromTo(insta, {
+            opacity: 0,
+            xPercent:-20,
+            skewX:"10deg",
+        },
+            {
+                opacity: 1,
+                xPercent:0,
+                delay:1,
+                skewX:"0",
+                duration: 1,
+                scrollTrigger:{
+                  trigger:bottom||bottomDom,
+                  start:"top 80%",
+                }
+            })
+        gsap.fromTo(twitter, {
+            opacity: 0,
+            xPercent:-20,
+            skewX:"10deg",
+        },
+            {
+                opacity: 1,
+                xPercent:0,
+                delay:1.2,
+                skewX:"0",
+                duration: 1,
+                scrollTrigger:{
+                  trigger:bottom||bottomDom,
+                  start:"top 80%",
+                }
+            })
+        gsap.fromTo(behance, {
+            opacity: 0,
+            xPercent:-20,
+            skewX:"10deg",
+        },
+            {
+                opacity: 1,
+                xPercent:0,
+                delay:1.4,
+                skewX:"0",
+                duration: 1,
+                scrollTrigger:{
+                  trigger:bottom||bottomDom,
+                  start:"top 80%",
+                }
+            })
+    }, []);
+
   return (
     <footer className='footer'>
       <div className='top'>
@@ -19,23 +82,23 @@ const Footer = () => {
 
       <div className='line'></div>
 
-      <div className='bottom'>
+      <div ref={bottomRef} className='bottom'>
         <div className='box'>
           <div className='logo'></div>
           <div className='socials'>
-            <div className='item'>
+            <div className='item insta'>
               <div className='icon'>
                 <FaInstagram />
               </div>
               <span>Instagram</span>
             </div>
-            <div className='item'>
+            <div className='item twitter'>
               <div className='icon'>
                 <FiTwitter />
               </div>
               <span>Twitter</span>
             </div>
-            <div className='item'>
+            <div className='item behance'>
               <div className='icon'>
                 <FaBehance />
               </div>
